ed955fb4216b233336df62fadca4fa54a855dbc4
phkug <46046996+phkug@users.noreply.github.com>
2021-05-17 09:39:31 +0200

    Add files via upload

---
+++ src/commons/Data.cpp
@@ -0,0 +1,256 @@
+/*-------------------------------------------------------------------------------
+  This file is part of generalized-random-forest.
+
+  grf is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grf is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grf. If not, see <http://www.gnu.org/licenses/>.
+ #-------------------------------------------------------------------------------*/
+
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+
+#include "Data.h"
+
+Data::Data() :
+    num_rows(0),
+    num_cols(0),
+    external_data(true),
+    index_data(0),
+    max_num_unique_values(0),
+    outcome_index(),
+    treatment_index(),
+    instrument_index(),
+    weight_index() {}
+
+Data::~Data() {
+  if (index_data != 0) {
+    delete[] index_data;
+  }
+}
+
+bool Data::load_from_file(std::string filename) {
+  bool result;
+
+  // Open input file
+  std::ifstream input_file;
+  input_file.open(filename);
+  if (!input_file.good()) {
+    throw std::runtime_error("Could not open input file.");
+  }
+
+  // Count number of rows
+  size_t line_count = 0;
+  std::string line;
+  std::string first_line;
+  while (getline(input_file, line)) {
+    if (line_count == 0) {
+      first_line = line;
+    }
+    ++line_count;
+  }
+
+  num_rows = line_count;
+  input_file.close();
+  input_file.open(filename);
+
+  // Find out if comma, semicolon or whitespace seperated and call appropriate method
+  if (first_line.find(",") != std::string::npos) {
+    result = load_from_other_file(input_file, first_line, ',');
+  } else if (first_line.find(";") != std::string::npos) {
+    result = load_from_other_file(input_file, first_line, ';');
+  } else {
+    result = load_from_whitespace_file(input_file, first_line);
+  }
+
+  external_data = false;
+  input_file.close();
+  return result;
+}
+
+bool Data::load_from_whitespace_file(std::ifstream& input_file, std::string first_line) {
+  // Read the first line to determine the number of columns.
+  std::string dummy_token;
+  std::stringstream first_line_stream(first_line);
+  while (first_line_stream >> dummy_token)  {
+    num_cols++;
+  }
+
+  // Read the entire contents.
+  reserve_memory();
+  bool error = false;
+  std::string line;
+  size_t row = 0;
+  while (getline(input_file, line)) {
+    double token;
+    std::stringstream line_stream(line);
+    size_t column = 0;
+    while (line_stream >> token) {
+      set(column, row, token, error);
+      ++column;
+    }
+    if (column > num_cols) {
+      throw std::runtime_error("Could not open input file. Too many columns in a row.");
+    } else if (column < num_cols) {
+      throw std::runtime_error("Could not open input file. Too few columns in a row. Are all values numeric?");
+    }
+    ++row;
+  }
+  num_rows = row;
+  return error;
+}
+
+bool Data::load_from_other_file(std::ifstream& input_file, std::string first_line, char seperator) {
+  // Read the first line to determine the number of columns.
+  std::string dummy_token;
+  std::stringstream first_line_stream(first_line);
+  while (getline(first_line_stream, dummy_token, seperator)) {
+    num_cols++;
+  }
+
+  // Read the entire contents.
+  reserve_memory();
+  bool error = false;
+  std::string line;
+  size_t row = 0;
+  while (getline(input_file, line)) {
+    std::string token_string;
+    double token;
+    std::stringstream line_stream(line);
+    size_t column = 0;
+    while (getline(line_stream, token_string, seperator)) {
+      std::stringstream token_stream(token_string);
+      token_stream >> token;
+      set(column, row, token, error);
+      ++column;
+    }
+    ++row;
+  }
+  num_rows = row;
+  return error;
+}
+
+void Data::set_outcome_index(size_t index) {
+  this->outcome_index = index;
+  disallowed_split_variables.insert(index);
+}
+
+void Data::set_treatment_index(size_t index) {
+  this->treatment_index = index;
+  disallowed_split_variables.insert(index);
+}
+
+void Data::set_instrument_index(std::vector<size_t> index) {
+  this->instrument_index = index;
+  for (auto it = index.begin(); it != index.end(); ++it)
+	  disallowed_split_variables.insert(*it);
+}
+
+void Data::set_weight_index(size_t index) {
+    this->weight_index = index;
+    disallowed_split_variables.insert(index);
+}
+
+void Data::get_all_values(std::vector<double>& all_values, const std::vector<size_t>& samples, size_t var) const {
+  all_values.reserve(samples.size());
+  for (size_t i = 0; i < samples.size(); ++i) {
+    all_values.push_back(get(samples[i], var));
+  }
+  std::sort(all_values.begin(), all_values.end());
+  all_values.erase(unique(all_values.begin(), all_values.end()), all_values.end());
+}
+
+size_t Data::get_index(size_t row, size_t col) const {
+  return index_data[col * num_rows + row];
+}
+
+void Data::sort() {
+  // Reserve memory
+  index_data = new size_t[num_cols * num_rows];
+
+  // For all columns, get unique values and save index for each observation
+  for (size_t col = 0; col < num_cols; ++col) {
+
+    // Get all unique values
+    std::vector<double> unique_values(num_rows);
+    for (size_t row = 0; row < num_rows; ++row) {
+      unique_values[row] = get(row, col);
+    }
+    std::sort(unique_values.begin(), unique_values.end());
+    unique_values.erase(unique(unique_values.begin(), unique_values.end()), unique_values.end());
+
+    // Get index of unique value
+    for (size_t row = 0; row < num_rows; ++row) {
+      size_t idx = std::lower_bound(unique_values.begin(), unique_values.end(), get(row, col)) - unique_values.begin();
+      index_data[col * num_rows + row] = idx;
+    }
+
+    // Save unique values
+    unique_data_values.push_back(unique_values);
+    if (unique_values.size() > max_num_unique_values) {
+      max_num_unique_values = unique_values.size();
+    }
+  }
+}
+
+double Data::get_unique_data_value(size_t var, size_t index) const {
+  return unique_data_values[var][index];
+}
+
+size_t Data::get_num_unique_data_values(size_t var) const {
+  return unique_data_values[var].size();
+}
+
+size_t Data::get_num_cols() const {
+  return num_cols;
+}
+
+size_t Data::get_num_rows() const {
+  return num_rows;
+}
+
+size_t Data::get_max_num_unique_values() const {
+  return max_num_unique_values;
+}
+
+size_t Data::get_number_instruments() const {
+	return instrument_index.value().size();
+}
+
+double Data::get_outcome(size_t row) const {
+  return get(row, outcome_index.value());
+}
+
+double Data::get_treatment(size_t row) const {
+  return get(row, treatment_index.value());
+}
+
+Eigen::VectorXd Data::get_instrument(size_t row) const {
+  Eigen::VectorXd instrument_values(instrument_index.value().size());
+  for (size_t i = 0; i < instrument_index.value().size(); i++) {
+	  instrument_values(i) = get(row, instrument_index.value().at(i));
+  }
+  return instrument_values; 
+}
+
+double Data::get_weight(size_t row) const {
+  if(weight_index.has_value()) {
+    return get(row, weight_index.value());
+  } else {
+    return 1.0;
+  }
+}
+
+const std::set<size_t>& Data::get_disallowed_split_variables() const {
+  return disallowed_split_variables;
+}
---
+++ src/forest/ForestTrainer.cpp
@@ -0,0 +1,140 @@
+/*-------------------------------------------------------------------------------
+  This file is part of generalized random forest (grf).
+
+  grf is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grf is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grf. If not, see <http://www.gnu.org/licenses/>.
+ #-------------------------------------------------------------------------------*/
+
+#include <algorithm>
+#include <ctime>
+#include <future>
+#include <stdexcept>
+#include <string>
+
+#include "commons/utility.h"
+#include "ForestTrainer.h"
+
+ForestTrainer::ForestTrainer(std::shared_ptr<RelabelingStrategy> relabeling_strategy,
+                             std::shared_ptr<SplittingRuleFactory> splitting_rule_factory,
+                             std::shared_ptr<OptimizedPredictionStrategy> prediction_strategy) :
+    tree_trainer(relabeling_strategy,
+                 splitting_rule_factory,
+                 prediction_strategy) {}
+
+const Forest ForestTrainer::train(const Data* data,
+                                  const ForestOptions& options) const {
+  size_t num_samples = data->get_num_rows();
+  uint num_trees = options.get_num_trees();
+
+  // Ensure that the sample fraction is not too small and honesty fraction is not too extreme.
+  const TreeOptions& tree_options = options.get_tree_options();
+  bool honesty = tree_options.get_honesty();
+  double honesty_fraction = tree_options.get_honesty_fraction();
+  if ((size_t) num_samples * options.get_sample_fraction() < 1) {
+    throw std::runtime_error("The sample fraction is too small, as no observations will be sampled.");
+  } else if (honesty && ((size_t) num_samples * options.get_sample_fraction() * honesty_fraction < 1
+             || (size_t) num_samples * options.get_sample_fraction() * (1-honesty_fraction) < 1)) {
+    throw std::runtime_error("The honesty fraction is too close to 1 or 0, as no observations will be sampled.");
+  }
+
+  uint num_groups = (uint) num_trees / options.get_ci_group_size();
+
+  std::vector<uint> thread_ranges;
+  split_sequence(thread_ranges, 0, num_groups - 1, options.get_num_threads());
+
+  std::vector<std::future<std::vector<std::shared_ptr<Tree>>>> futures;
+  futures.reserve(thread_ranges.size());
+
+  std::vector<std::shared_ptr<Tree>> trees;
+  trees.reserve(num_trees);
+
+  for (uint i = 0; i < thread_ranges.size() - 1; ++i) {
+    size_t start_index = thread_ranges[i];
+    size_t num_trees_batch = thread_ranges[i + 1] - start_index;
+
+    futures.push_back(std::async(std::launch::async,
+                                 &ForestTrainer::train_batch,
+                                 this,
+                                 start_index,
+                                 num_trees_batch,
+                                 data,
+                                 options));
+  }
+
+  for (auto& future : futures) {
+    std::vector<std::shared_ptr<Tree>> thread_trees = future.get();
+    trees.insert(trees.end(), thread_trees.begin(), thread_trees.end());
+  }
+
+  return Forest::create(trees, options, data);
+}
+
+std::vector<std::shared_ptr<Tree>> ForestTrainer::train_batch(
+    size_t start,
+    size_t num_trees,
+    const Data* data,
+    const ForestOptions& options) const {
+  size_t ci_group_size = options.get_ci_group_size();
+
+  std::mt19937_64 random_number_generator(options.get_random_seed() + start);
+  std::uniform_int_distribution<uint> udist;
+  std::vector<std::shared_ptr<Tree>> trees;
+
+  if (ci_group_size == 1) {
+    trees.reserve(num_trees);
+  } else {
+    trees.reserve(num_trees * ci_group_size);
+  }
+
+  for (size_t i = 0; i < num_trees; i++) {
+    uint tree_seed = udist(random_number_generator);
+    RandomSampler sampler(tree_seed, options.get_sampling_options());
+
+    if (ci_group_size == 1) {
+      std::shared_ptr<Tree> tree = train_tree(data, sampler, options);
+      trees.push_back(tree);
+    } else {
+      std::vector<std::shared_ptr<Tree>> group = train_ci_group(data, sampler, options);
+      trees.insert(trees.end(), group.begin(), group.end());
+    }
+  }
+  return trees;
+}
+
+std::shared_ptr<Tree> ForestTrainer::train_tree(const Data* data,
+                                                RandomSampler& sampler,
+                                                const ForestOptions& options) const {
+  std::vector<size_t> clusters;
+  sampler.sample_clusters(data->get_num_rows(), options.get_sample_fraction(), clusters);
+  return tree_trainer.train(data, sampler, clusters, options.get_tree_options());
+}
+
+std::vector<std::shared_ptr<Tree>> ForestTrainer::train_ci_group(const Data* data,
+                                                                 RandomSampler& sampler,
+                                                                 const ForestOptions& options) const {
+  std::vector<std::shared_ptr<Tree>> trees;
+
+  std::vector<size_t> clusters;
+  sampler.sample_clusters(data->get_num_rows(), 0.5, clusters);
+
+  double sample_fraction = options.get_sample_fraction();
+  for (size_t i = 0; i < options.get_ci_group_size(); ++i) {
+    std::vector<size_t> cluster_subsample;
+    sampler.subsample(clusters, sample_fraction * 2, cluster_subsample);
+
+    std::shared_ptr<Tree> tree = tree_trainer.train(data,
+        sampler, cluster_subsample, options.get_tree_options());
+    trees.push_back(tree);
+  }
+  return trees;
+}
